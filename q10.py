44b6272deeb793a67a9ba47b22b4c740b37a15c6
JonathanSaless <jonathansales1998@gmail.com>
2024-12-05 22:40:58 -0300

    update

--- q10.py
+++ q10.py
@@ -1,19 +1,24 @@
+import sys
+
+# defino um novo limite de recursão para que o código funcione corretamente em valores altos de n
+sys.setrecursionlimit(200000)
+
 def josephus(n, k):
-    resultado = 0  # A posição base quando n = 1 (zero-indexed)
-    
-    # Iteração para calcular a posição final quando n > 1
-    for i in range(2, n + 1):
-        resultado = (resultado + k) % i
-    
-    # A resposta é a posição final convertida para um índice de 1 (baseada em 1)
-    return resultado + 1
+    # Caso base: se houver uma pessoa, ela será a sobrevivente
+    if n == 1:
+        return 0
+    # Caso recorrente: calcula a posição do sobrevivente para n-1 pessoas
+    else:
+        return (josephus(n - 1, k) + k) % n
 
 def main():
     NC = int(input())  # Número de casos de teste
-    for case_num in range(1, NC + 1):
-        n, k = map(int, input().split())  # Leitura de n e k
-        result = josephus(n, k)
-        print(f"Case {case_num}: {result}")
+    for case_ in range(1, NC + 1):
+        primeira_linha = input()
+        n, k = (int(numero) for numero in primeira_linha.split())
+        
+        sobrevivente = josephus(n, k) + 1
+        print(f"Case {case_}: {sobrevivente}")
 
 if __name__ == "__main__":
     main()
